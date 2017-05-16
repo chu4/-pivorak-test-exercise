19bb79163e44df830512a6d84dee3b24365496fa
chu4 <antonchupack@gmail.com>
2017-05-16 13:28:10 +0300

    added reading/writing the admin/user info from/to yml files

--- #pivorak_test_problem.rb
+++ #pivorak_test_problem.rb
@@ -1,15 +1,37 @@
-admin = {root: "123"}
-user = {chu: "123", abc: "234"}
+require 'yaml'
+
+#loads logins and passwords of admins from file into a hash
+admin = YAML.load_file('admin.yml')
+
+#loads logins and passwords of users from a file into a hash
+user = YAML.load_file('user.yml')
+
+puts admin,user
 
 puts "Do you want to login or register?" #type in login or register
 first_entry = gets.chomp
 
 case first_entry
 when "login"  #case of logining
-  puts "Enter login:"
-  login = gets.chomp
-  puts "Enter password:"
-  pass = gets.chomp
+  login = nil
+  while login == nil
+    puts "Enter login:"
+    login1 = gets.chomp
+    puts "Enter password:"
+    pass = gets.chomp
+    if admin.key?(login1.to_sym) && pass == admin[login1.to_sym] then  #checking if the login and password are in admin hash
+      login = login1
+      puts "Welcome admin #{login1}"
+    elsif user.key?(login1.to_sym) && pass == user[login1.to_sym] then  #checking if login and password are in user hash
+      login = login1
+      puts "Welcome user #{login1}"
+    else
+      puts "Login and/or password are not right." # in case of either wrong login or password promts user to re-enter them
+    end
+  end
+
+
+
 when "register"           #case of registering (it is only possible to register regular user
   while login == nil      #not an admin). prompting user to enter correct login
     puts "Enter login:"
@@ -26,7 +48,7 @@ when "register"           #case of registering (it is only possible to register 
     pass1 = gets.chomp
     puts "Enter password one more time:"
     pass2 = gets.chomp
-  
+
     if pass1==pass2 then
       pass=pass1
     else
@@ -34,6 +56,7 @@ when "register"           #case of registering (it is only possible to register 
     end
   end
   user[login.to_sym] = pass #saving new user in user.hash
+  File.write('user.yml', user.to_yaml)
 else
   puts "invalid entry" # if the user didnt enter login nor register
 end
